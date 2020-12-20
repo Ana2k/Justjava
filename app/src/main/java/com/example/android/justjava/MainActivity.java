705bc4fffbb725df1c59a17e94b3bc9ea94f59cc
Ana2k <anushkachakra@gmail.com>
2020-12-20 12:39:07 +0530

    added scrolling and checkboxes

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,47 +3,73 @@ package com.example.android.justjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
 
 
 public class MainActivity extends AppCompatActivity {
-    int Order=0;
+    int Quantity=0,price=0,pricePerCoffee=10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void displayOrder(){
-        TextView price = (TextView) findViewById(R.id.quantity_text_view);
-        price.setText(""+Order);
-    }
 
-    public void displayPrice(int number){
+    public void IncreaseQuantity_1(View view){
+        Quantity++;
+        calculatePrice(Quantity,pricePerCoffee);
+        displayDetails(Quantity,price);
+    }
+    public void DecreaseQuantity_1(View view){
+        Quantity--;
+        if(Quantity<0)Quantity=0;
+        calculatePrice(Quantity,pricePerCoffee);
+        displayDetails(Quantity,price);
+    }
+    public void displayDetails(int number,int price) {
+        TextView priceNum = (TextView) findViewById(R.id.quantity_text_view);
+        priceNum.setText("" + Quantity);
+
         TextView priceText = (TextView) findViewById(R.id.price_text_view);
-        priceText.setText("₹ "+number);
+        priceText.setText("₹ "+price);
     }
 
-    public void IncreaseOrder_1(View view){
-        Order++;
-        displayOrder();
-        displayPrice(Order*10);
+
+    public int calculatePrice(int Quantity,int pricePerCoffee){
+        price=Quantity*pricePerCoffee;
+        return price;
     }
-    public void DecreaseOrder_1(View view){
-        Order--;
-        if(Order<0)Order=0;
-        displayOrder();
-        displayPrice(Order*10);
+
+
+    public String createOrderSummary(){
+
+        int price=calculatePrice(Quantity,10);
+        String name = "Anushka";
+
+        CheckBox WhippedCream = findViewById(R.id.checkbox_whipped_cream);
+        boolean CheckBoxWhippedCream = WhippedCream.isChecked();
+
+        Log.v("CheckBox Activity:","Whipped Cream is"+CheckBoxWhippedCream);
+
+        String message="Name: "+name+
+                "\nQuantity: "+Quantity+
+                "\nAdd Whipped Cream?"+CheckBoxWhippedCream+
+                "\nTotal: "+price+"\n";
+        return message;
     }
-    public void displayMessage(View view) {
+
+    public void displayOrder(View view) {
         TextView messageText = (TextView) findViewById(R.id.message);
-        if(Order>0)
-            messageText.setText("Your order of ₹ "+ Order*10+" has been placed");
-        else
+        if (Quantity > 0) {
+            messageText.setText(createOrderSummary()+"Thank you!");
+        } else {
             messageText.setText("No order has been placed");
+        }
     }
+
+
 }
 
 
